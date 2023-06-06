9852ddf81735b46586ed77e46dcb42a23d93d84c
Vas2306 <vasilisa230679@gmail.com>
2023-06-06 23:49:34 +0200

    name

--- js/name.js
+++ js/name.js
@@ -1,7 +1,19 @@
-   const yourName = prompt('Введите ваше имя!');
+let yourName = prompt("Введите ваше имя!");
+yourName = yourName.toUpperCase();
+const numberLetter = prompt(
+  "Напишите номер буквы вашего имени, который вы хотите узнать"
+);
+const messageQuantity = "";
 const quantityLetter = yourName.length;
-const messageQuantity = confirm(`В вашем имени ${yourName} количество букв - ${quantityLetter}`);
-const numberLetter = prompt('Напишите номер буквы вашего имени, который вы хотите узнать');
-const number = numberLetter - 1;
-const letter = yourName[number];
-const yourLetter = confirm(`Введенному вами номеру буквы соответсвует буква ${letter}`);
+const yourLetter = "";
+if (yourName !== "" && numberLetter !== "" && numberLetter < quantityLetter) {
+  const number = numberLetter - 1;
+  const letter = yourName[number];
+  messageQuantity = confirm(
+    `В вашем имени ${yourName} количество букв - ${quantityLetter}, введенному вами номеру ${numberLetter} соответсвует буква ${letter}`
+  );
+} else {
+  messageQuantity = confirm(
+    `Вы ввели не все данные или неверные, попробуйте еще раз`
+  );
+}
